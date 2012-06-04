77992335e5df400a46a7ea305cc7f47dd4872e99
Tim Roediger <tim.roediger@ausaero.com.au>
2012-06-04 15:16:12 +1000

    Move module.php

---
+++ src/DojoModule/Module.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package    DojoModule
+ * @license    MIT
+ */
+namespace DojoModule;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+    
+    public function getServiceConfiguration()
+    {
+        return array(
+            'factories' => array(
+                'DojoModule\View\Helper\Dojo' => 'DojoModule\Service\DojoFactory',
+                'dojo_cli' => 'DojoModule\Service\CliFactory',                
+            )
+        );
+    }     
+}

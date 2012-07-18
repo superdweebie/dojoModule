0b839d7fdd4a2a4b09ea502de7dc7eae35d53e5c
Tim Roediger <tim.roediger@ausaero.com.au>
2012-07-19 09:27:38 +1000

    Refactoring

--- config/module.config.php
+++ config/module.config.php
@@ -1,10 +1,5 @@
 <?php
 return array(
-    'view_manager' => array(
-        'helper_map' => array(
-            'dojo' => 'DojoModule\View\Helper\Dojo'
-        ),
-    ),
     'dojo' => array(
         'activeDojoRoot' => 'release',
         'dojoRoots' => array(
@@ -12,15 +7,15 @@ return array(
             'release' => 'js/dojo_rel'
         ),
         'build' => array(
-            'profile' => 'data/dojoModule.profile.js',
+            'profile' => 'data/dojo-module.profile.js',
             'basePath' => '../',
             'releaseDir' => 'public/js/dojo_rel',
-            'action' => 'release', 
+            'action' => 'release',
             'cssOptimize' => 'comments',
             'optimize' => 'closure',
             'layerOptimize' => 'closure',
             'stripConsole' => 'all',
-            'selectorEngine' => 'acme', 
+            'selectorEngine' => 'acme',
             'packages' => array(
                 'dojo' => 'vendor/dojo/dojo',
                 'dijit' => 'vendor/dojo/dijit',
@@ -29,59 +24,26 @@ return array(
         ),
         'theme' => 'claro',
         'require' => array(
-            'parser',                
+            'dojo/parser'
         ),
-        'modules' => array(
-            'dojo' => array(
-                'name' => 'dojo/dojo'
+        'stylesheets' => array(
+            'dojo/parser' => array(
+                'dojo/resources/dojo.css',
+                'dijit/themes/[THEME]/[THEME].css'
             ),
-            'parser' => array(
-                'name' => 'dojo/parser',
-                'stylesheets' => array(
-                    'dojo/resources/dojo.css',
-                    'dijit/themes/[THEME]/[THEME].css'                         
-                )
-            ),                                                   
-            'borderContainer' => array(
-                'name' => 'dijit/layout/BorderContainer'
-            ),            
-            'contentPane' => array(
-                'name' => 'dijit/layout/ContentPane'
-            ),            
-            'button' => array(
-                'name' => 'dijit/form/Button'
-            ),                         
-            'enhancedGrid' => array(
-                'name' => 'dojox/grid/EnhancedGrid',
-                'stylesheets' => array(
-                    'dojox/grid/enhanced/resources/[THEME]/EnhancedGrid.css', 
-                    'dojox/grid/enhanced/resources/EnhancedGrid_rtl.css',                        
-                )                    
-            ),            
-            'checkedMultiSelect' => array(
-                'name' => 'dojox/form/CheckedMultiSelect',
-                'stylesheets' => array(
-                    'dojox/form/resources/CheckedMultiSelect.css',                  
-                )                    
-            ),            
-            'standby' => array(
-                'name' => 'dojox/widget/standby',
-                'stylesheets' => array(
-                    'dojox/widget/Standby/Standby.css',               
-                )                    
-            ),            
-            'uploader' => array(
-                'name' => 'dojox/form/Uploader',                   
-            ),            
-            'uploaderFilelist' => array(
-                'name' => 'dojox/form/uploader/FileList',
-                'stylesheets' => array(
-                    'dojox/form/resources/UploaderFileList.css',               
-                )                    
-            ),            
-            'uploaderHtml5' => array(
-                'name' => 'dojox/form/uploader/plugins/HTML5',              
-            ),                    
+            'dojox/grid/enhancedGrid' => array(
+                'dojox/grid/enhanced/resources/[THEME]/EnhancedGrid.css',
+                'dojox/grid/enhanced/resources/EnhancedGrid_rtl.css',
+            ),
+            'dojox/form/checkedMultiSelect' => array(
+                'dojox/form/resources/CheckedMultiSelect.css',
+            ),
+            'dojox/widget/standby' => array(
+                'dojox/widget/Standby/Standby.css',
+            ),
+            'dojox/form/uploaderFilelist' => array(
+                'dojox/form/resources/UploaderFileList.css',
+            ),
         ),
     ),
 );

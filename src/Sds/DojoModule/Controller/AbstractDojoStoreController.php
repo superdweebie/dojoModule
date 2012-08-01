a41fa58090b0e204d7d9ceba594d6e4dafbcc3f8
Tim Roediger <superdweebie@gmail.com>
2012-08-01 15:19:45 +1000

    Begin adding dojo store json rest controller

---
+++ src/Sds/DojoModule/Controller/AbstractDojoStoreController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AbstractDojoStoreController
+ *
+ * @author Team Roediger
+ */
+class AbstractDojoStoreController {
+    //put your code here
+}
+
+?>

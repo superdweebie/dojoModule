67f46b0105ddd4dbe926c75c5bd06ae0184bceb8
Tim Roediger <tim.roediger@ausaero.com.au>
2012-08-22 16:41:32 +1000

    Minor cleanup

--- tests/test.application.config.php
+++ tests/test.application.config.php
@@ -5,7 +5,7 @@ return array(
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
-            __DIR__ . '/test.module.config.php',
+            'vendor/superdweebie/dojo-module/tests/test.module.config.php',            
         ),
         'module_paths' => array(
             './vendor',

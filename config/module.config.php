d1e04276d22366e91ed00c23d6d18a819d88143a
Tim Roediger <superdweebie@gmail.com>
2012-09-07 06:38:27 +1000

    Removing Sds stuff that shouldn't be here

--- config/module.config.php
+++ config/module.config.php
@@ -21,7 +21,7 @@ return array(
             'build' => array(
                 'profilePath' => 'data/dojo-module.profile.js',
                 'basePath' => '../',
-                'releaseDir' => 'public/js/dojo_rel',
+                'releaseDir' => 'public/dojo_rel',
                 'action' => 'release',
                 'cssOptimize' => 'comments',
                 'optimize' => 'closure',
@@ -33,13 +33,6 @@ return array(
                     'dojo' => 'vendor/dojo/dojo',
                     'dijit' => 'vendor/dojo/dijit',
                     'dojox' => 'vendor/dojo/dojox',
-                    'Sds' => 'vendor/dojo/Sds'
-                ),
-                'plugins' => array(
-                    'Sds/ConfigManager/ConfigReady' => 'Sds/Build/Plugin/ConfigReady',
-                    'Sds/ServiceManager/SharedServiceManager' => 'Sds/Build/Plugin/SharedServiceManager',
-                    'Sds/ServiceManager/Shared/GetObject' => 'Sds/Build/Plugin/CreateObject',
-                    'Sds/ServiceManager/Shared/CreateObject' => 'Sds/Build/Plugin/CreateObject'
                 ),
                 'layers' => array(
                     'dojo/dojo' => array(
@@ -49,11 +42,6 @@ return array(
                             'dojo/parser'
                         )
                     ),
-                    'Sds/AuthModule' => array(
-                        'include' => array(
-                            'Sds/AuthModule/AuthController'
-                        )
-                    )
                 ),
             ),
             'stylesheets' => array(
